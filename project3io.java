cf5c7e9bd22d43150bd6a4d5d30df87a4fed0cce
nosremel <lemersonjan@gmail.com>
2017-01-25 20:05:32 -0800

    First commit

---
+++ project3io.java
@@ -0,0 +1,19 @@
+class project3io{
+		public static void main(String[]args){
+		boolean foundUSTP = false ;
+
+		
+		for ( String i : args ){
+
+			System.out.println(i);
+
+			}
+
+				System.out.print( "\nThere are " + args.length + " arguments");
+
+				
+		
+	}	
+
+}
+
